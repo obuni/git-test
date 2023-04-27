7fe6f303a70e094dedb3ff32f92da8b63f970d9c
obuni <dksmfody@naver.com>
2023-04-27 23:12:05 +0900

    modi

--- quasar.config.js
+++ quasar.config.js
@@ -24,7 +24,7 @@ module.exports = configure(function (ctx) {
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
     boot: [
       
-      'axios',
+      'axios','title'
     ],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css
@@ -48,7 +48,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
@@ -83,6 +83,7 @@ module.exports = configure(function (ctx) {
       open: true // opens browser window automatically
     },
 
+
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-framework
     framework: {
       config: {},
@@ -98,7 +99,7 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Notify', 'Dialog','LocalStorage',]
     },
 
     // animations: 'all', // --- includes all animations
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,11 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/vue', component: () => import('pages/VueEx.vue') },
+      { path: '/shop', component: () => import('pages/ShoppingList.vue') },
+      { path: '/todo', component: () => import('pages/Todo.vue') },
+      { path: '/calendar', component: () => import('pages/Calendar.vue') },
     ]
   },
 
